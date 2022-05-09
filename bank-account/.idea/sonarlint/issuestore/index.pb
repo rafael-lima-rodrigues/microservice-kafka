736137e4fef8812fc28c83ca14c6eedfa7ec2c46
Rafael Rodrigues <rafaa.verdao@gmail.com>
2022-05-09 00:11:12 -0300

    Create Message, CommandDispatcher and AccountCommandDispatcher to implements Mediator Pattern

---
+++ bank-account/.idea/sonarlint/issuestore/index.pb
@@ -0,0 +1,3 @@
+
+E
+account.query/pom.xml,9\a\9a10aa09b3f041ec0124ad53c7a2622dd2077540
---
+++ cqrs-es/cqrs.core/src/main/java/com/techbank/cqrs/core/domain/AggregateRoot.java
@@ -0,0 +1,59 @@
+package com.techbank.cqrs.core.domain;
+
+import com.techbank.cqrs.core.events.BaseEvent;
+import lombok.Getter;
+import lombok.Setter;
+import org.springframework.stereotype.Service;
+
+import java.text.MessageFormat;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
+
+public abstract class AggregateRoot {
+    @Getter
+    protected String id;
+
+    @Getter
+    @Setter
+    private int version = 1;
+
+    private final List<BaseEvent> changes = new ArrayList<>();
+    private final Logger logger = Logger.getLogger(AggregateRoot.class.getName());
+
+    public List<BaseEvent> getUncommittedChanges(){
+        return this.changes;
+    }
+
+    public void markChangesAsCommitted(){
+        this.changes.clear();
+    }
+
+    protected void applyChange(BaseEvent event, Boolean isNewEvent){
+        try {
+            var method = getClass().getDeclaredMethod("apply", event.getClass());
+            method.setAccessible(true);
+            method.invoke(this, event);
+        } catch (NoSuchMethodException e) {
+            logger.log(
+                    Level.WARNING,
+                    MessageFormat.format(
+                            "The apply method was not in the aggregate for {0}",
+                            event.getClass().getName()
+                    ));
+        } catch (Exception e){
+            logger.log(Level.SEVERE,"Error applying event to aggregate", e);
+        }
+    }
+
+    public void raiseEvent(BaseEvent event){
+        applyChange(event, true);
+    }
+
+    public void replayEvents(Iterable<BaseEvent> events){
+        events.forEach(event -> applyChange(event, false));
+    }
+
+}
